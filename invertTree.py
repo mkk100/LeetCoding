ea603ba19b287bee28662776ac28f4f8b3c42888
mkk100 <tommychimes2003@gmail.com>
2023-09-14 09:35:26 -0700

    Create invertTree.py

---
+++ invertTree.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
