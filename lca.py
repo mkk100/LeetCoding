6dec7afb6910dce7a0f28b74e994c34e4fb5b231
mkk100 <tommychimes2003@gmail.com>
2023-09-15 09:22:06 -0700

    Create lca.py

---
+++ lca.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        cur = root
+        while cur:
+            if cur.val < p.val and cur.val < q.val:
+                cur = cur.right
+            elif cur.val > p.val and cur.val > q.val:
+                cur = cur.left
+            else:
+                return cur
+        
