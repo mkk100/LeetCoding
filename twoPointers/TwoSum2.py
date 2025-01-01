76529642c4f70294ebeea828614091e78a90cb9e
mkk100 <tommychimes2003@gmail.com>
2024-12-31 16:11:06 -0800

    Add solution for Two Sum II problem with time and space complexity comments

---
+++ twoPointers/TwoSum2.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        l,r = 0, len(numbers) - 1
+
+        while l <= r:
+            total = numbers[l] + numbers[r] 
+            if total > target:
+                r -= 1
+            elif total < target:
+                l += 1
+            elif total == target:
+                return [l + 1, r + 1]
+
+# Time - O(N)
+# Space - O(1)
