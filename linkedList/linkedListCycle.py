b38c753eaa9c6b74c369c44a220ebce7d9e3dc90
mkk100 <tommychimes2003@gmail.com>
2025-01-02 15:47:05 -0800

    linkedlist cycle

---
+++ linkedList/linkedListCycle.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        dummy = ListNode()
+        dummy.next = head
+        slow, fast = dummy, dummy
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+        return False
