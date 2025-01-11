e0e4010257555afbd71c507f7e11239b5b776570
mkk100 <tommychimes2003@gmail.com>
2025-01-11 13:22:52 -0800

    Add solutions for sorting an array by parity, finding the duplicate number, and sorting colors using two pointers

---
+++ slidingWindow/sortByParity.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        l, r = 0, 1
+        for r in range(len(nums)):
+            if nums[r] % 2 == 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+            r += 1
+        return nums
+# Time: O(N)
