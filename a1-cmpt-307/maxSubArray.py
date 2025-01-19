6caf1c50b7964f57dcbf2c53fdcc17c530565429
mkk100 <tommychimes2003@gmail.com>
2025-01-18 20:38:42 -0800

    Add optimized solution for maximum subarray sum using divide and conquer

--- a1-cmpt-307/maxSubArray.py
+++ a1-cmpt-307/maxSubArray.py
@@ -1,4 +1,4 @@
-def maxSubArray(self, nums: List[int]) -> int:
+def maxSubArray(self, nums: List[int]) -> int: # Brute Force
 
         ans = -inf
         for i in range(len(nums)):
@@ -7,4 +7,16 @@ def maxSubArray(self, nums: List[int]) -> int:
                 cur += nums[j]
                 ans = max(cur, ans)
         return ans
-        
+
+class Solution:
+    def maxSubArray(self, nums):
+        def maxSubArray(A, L, R):
+            if L > R: return -inf
+            mid, left_sum, right_sum, cur_sum = (L + R) // 2, 0, 0, 0
+            for i in range(mid-1, L-1, -1):
+                left_sum = max(left_sum, cur_sum := cur_sum + A[i])
+            cur_sum = 0
+            for i in range(mid+1, R+1):
+                right_sum = max(right_sum, cur_sum := cur_sum + A[i])
+            return max(maxSubArray(A, L, mid-1), maxSubArray(A, mid+1, R), left_sum + A[mid] + right_sum)
+        return maxSubArray(nums, 0, len(nums)-1)
