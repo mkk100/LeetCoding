ee45ba7c6686b21945c1f39e84b8ca54d4356c74
mkk100 <tommychimes2003@gmail.com>
2025-01-27 10:59:28 -0800

    heapSort

--- a1-cmpt-307/heap.py
+++ a1-cmpt-307/heap.py
@@ -18,4 +18,10 @@ def build_max_heap(a):
     arrLen = len(a)
     for i in range(arrLen//2,0,-1):
         max_heapify(a, arrLen, i)
+
+def heapSort(a):
+    build_max_heap(a)
     
+    for i in range(len(a) - 1, 0, -1):
+        a[0], a[i] = a[i], a[0]
+        max_heapify(a, i, 0)
