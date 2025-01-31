2cef37e82146ded6c27a98e676942f3744415738
mkk100 <tommychimes2003@gmail.com>
2025-01-31 10:49:31 -0800

    min_heapify and min_heap

--- a1-cmpt-307/heap.py
+++ a1-cmpt-307/heap.py
@@ -11,7 +11,7 @@ def max_heapify(arr, size, i):
     
     if largest != i:
         arr[largest], arr[i] = arr[i], arr[largest]
-        max_heapify(arr, size, i)
+        max_heapify(arr, size, largest)
     
     
 def build_max_heap(a):
@@ -25,3 +25,23 @@ def heapSort(a):
     for i in range(len(a) - 1, 0, -1):
         a[0], a[i] = a[i], a[0]
         max_heapify(a, i, 0)
+        
+        
+        
+def min_heapify(arr,size, i):
+    l = 2 * i
+    r = 2 * i + 1
+    smallest = i
+    
+    if arr[l] < arr[smallest]:
+        smallest = l
+    elif arr[r] < arr[smallest]:
+        smallest = r
+    
+    if smallest != i:
+        arr[smallest], arr[i] = arr[i], arr[smallest]
+        min_heapify(arr, size, smallest)
+
+def build_min_heap(a):
+    for i in range(len(a)//2, 0,-1):
+        min_heapify(a, len(a),i)
