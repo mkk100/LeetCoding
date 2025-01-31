6e0261d8d3eeced6ad94ec0b298fea574dd62623
mkk100 <tommychimes2003@gmail.com>
2025-01-31 12:32:18 -0800

    minHeapSort doesn't work

--- a1-cmpt-307/heap.py
+++ a1-cmpt-307/heap.py
@@ -1,50 +1,61 @@
 def max_heapify(arr, size, i):
     l = 2 * i
     r = 2 * i + 1
-    
+
     largest = i
-    
-    if arr[i] < arr[l]:
+
+    if l < size and arr[i] < arr[l]:
         largest = l
-    elif arr[i] < arr[r]:
+    elif r < size and arr[i] < arr[r]:
         largest = r
-    
+
     if largest != i:
         arr[largest], arr[i] = arr[i], arr[largest]
         max_heapify(arr, size, largest)
-    
-    
+
+
 def build_max_heap(a):
     arrLen = len(a)
-    for i in range(arrLen//2,0,-1):
+    for i in range(arrLen // 2 - 1, -1, -1):
         max_heapify(a, arrLen, i)
 
-def heapSort(a):
+
+def maxHeapSort(a):
     build_max_heap(a)
-    
+
     for i in range(len(a) - 1, 0, -1):
         a[0], a[i] = a[i], a[0]
         max_heapify(a, i, 0)
-        
-        
-        
-def min_heapify(arr,size, i):
+
+
+def min_heapify(arr, size, i):
     l = 2 * i
     r = 2 * i + 1
     smallest = i
-    
-    if arr[l] < arr[smallest]:
+
+    if l < size and arr[l] < arr[smallest]:
         smallest = l
-    elif arr[r] < arr[smallest]:
+    elif r < size and arr[r] < arr[smallest]:
         smallest = r
-    
+
     if smallest != i:
         arr[smallest], arr[i] = arr[i], arr[smallest]
         min_heapify(arr, size, smallest)
 
+
 def build_min_heap(a):
-    for i in range(len(a)//2, 0,-1):
-        min_heapify(a, len(a),i)
-        
-def heap_extract_min(a):
-    return a[0]
+    for i in range((len(a) // 2) - 1, -1, -1):
+        min_heapify(a, len(a), i)
+
+
+def minHeapSort(a):
+    build_min_heap(a)
+
+    for i in range(len(a) - 1, 0, -1):
+        a[0], a[i] = a[i], a[0]
+        min_heapify(a, i, 0)
+
+
+a = [10, 7, 11, 5, 4, 13]
+minHeapSort(a)
+print(a)
