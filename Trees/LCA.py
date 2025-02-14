92c922cb8f9e144c13fe6b10575800a9b9e5a7cb
mkk100 <tommychimes2003@gmail.com>
2025-02-14 15:37:43 -0800

    Add implementation for finding the lowest common ancestor in a binary search tree

---
+++ Trees/LCA.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lowestCommonAncestor(
+        self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
+    ) -> "TreeNode":
+        if not root:
+            return None
+
+        while root:
+            if p.val > root.val and q.val > root.val:
+                root = root.right
+            elif p.val < root.val and q.val < root.val:
+                root = root.left
+            else:
+                return root
