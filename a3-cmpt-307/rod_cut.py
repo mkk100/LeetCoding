6de951cc2dba623db36bd39bcdc21a6eb9076a52
mkk100 <tommychimes2003@gmail.com>
2025-02-21 16:03:58 -0800

    memoized

--- a3-cmpt-307/rod_cut.py
+++ a3-cmpt-307/rod_cut.py
@@ -1,27 +1,22 @@
-# Python program to find maximum
-# profit from rod of size n 
-
-def cutRodRecur(i, price):
-    
-    # Base case
-    if i == 0:
+def cutRod(i, price, memo):
+    if i in memo:
+        return memo[i]
+    if i <= 0:
         return 0
-    
     ans = 0
-
-    # Find maximum value for each cut.
-    # Take value of rod of length j, and 
-    # recursively find value of rod of 
-    # length (i-j).
-    for j in range(1, i + 1):
-        ans = max(ans, price[j - 1] + cutRodRecur(i - j, price))
+    for j in range(1, i):
+        res = price[j-1] + cutRod(i - j, price, memo)
+        ans = max(ans, res)
+    memo[i] = ans
 
     return ans
 
+
 def cutRod(price):
     n = len(price)
+    memo = {}
+    return cutRod(n, price, memo)
 
-    return cutRodRecur(n, price)
 
 if __name__ == "__main__":
     price = [1, 5, 8, 9, 10, 17, 17, 20]
