f0deb769a454da61bf21e5a7f6af21475c3ddfe4
mkk100 <tommychimes2003@gmail.com>
2026-04-29 11:05:39 -0700

    sliding window

---
+++ 2026/DSA_Review/Sliding Window/minimumSizeSubarraySum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        l = 0
+        cur_sum = 0
+        min_window = float("inf")
+        for r in range(len(nums)):
+            cur_sum += nums[r]
+            while cur_sum >= target:
+                min_window = min(min_window, r - l + 1)
+                cur_sum -= nums[l]
+                l += 1    
+        
+        return 0 if min_window == float('inf') else min_window
+    # need to return to the sliding windows and graphs
