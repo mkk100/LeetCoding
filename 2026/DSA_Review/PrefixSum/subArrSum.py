67ea5882217bcba9e1d277c50b6b6035e138518e
mkk100 <tommychimes2003@gmail.com>
2026-05-25 13:50:51 -0700

    prefix sum

---
+++ 2026/DSA_Review/PrefixSum/subArrSum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        hashMap = {0:1}
+        res = 0
+        curSum = 0
+        for n in nums:
+            curSum += n
+            res += hashMap.get(curSum - k, 0) 
+            hashMap[curSum] = hashMap.get(curSum, 0) + 1
+        return res
+            
