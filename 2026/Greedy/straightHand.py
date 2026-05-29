8417be1f6adb4822543904048d8b6e9c0781b22a
mkk100 <tommychimes2003@gmail.com>
2026-05-29 11:41:48 -0700

    greedy

---
+++ 2026/Greedy/straightHand.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize:
+            return False
+
+        count = Counter(hand)
+        minHeap = list(count.keys())
+        heapq.heapify(minHeap)
+
+        while minHeap:
+            first = minHeap[0]
+            for i in range(first, first + groupSize):
+                if count[i] == 0:
+                    return False
+                count[i] -= 1
+                if count[i] == 0:
+                    if i != minHeap[0]:
+                        return False
+                    heapq.heappop(minHeap)
+        
+        return True
