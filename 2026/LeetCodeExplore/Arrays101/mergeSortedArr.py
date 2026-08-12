19f7e57bef323ad7b5fd1dd78e312f8628270fe7
mkk100 <tommychimes2003@gmail.com>
2026-08-12 12:09:57 -0700

    got it :)

--- 2026/LeetCodeExplore/Arrays101/mergeSortedArr.py
+++ 2026/LeetCodeExplore/Arrays101/mergeSortedArr.py
@@ -22,4 +22,33 @@ class Solution(object):
                 i -= 1
         
         
-# idk bro, can't solve
+# the problem with this approach is that once you finish swapping 
+
+# correct approach
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        i = m - 1
+        j = n - 1
+        k = len(nums1) - 1
+        
+        while j >= 0:
+            print(nums1[i], nums2[j],nums1[k], nums1)
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i] 
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            
+            k -= 1
+        
+
+        
