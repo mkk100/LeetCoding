8636e6f61453f65883b14054e309a9c41e3db029
mkk100 <tommychimes2003@gmail.com>
2026-08-13 07:24:31 -0700

    remDup

---
+++ 2026/LeetCodeExplore/Arrays101/removeDupsFromSortedArr.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l = 0
+        for r in range(len(nums)):
+            if nums[l] != nums[r]:
+                l += 1
+                nums[l] = nums[r]
+        return l + 1
