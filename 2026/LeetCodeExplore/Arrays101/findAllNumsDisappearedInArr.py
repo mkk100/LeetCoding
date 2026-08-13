9ac26a06cf70239c151f1535b8206797bb2daf25
mkk100 <tommychimes2003@gmail.com>
2026-08-13 09:56:51 -0700

    easies

---
+++ 2026/LeetCodeExplore/Arrays101/findAllNumsDisappearedInArr.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        res = []
+        for n in nums:
+            i = abs(n) - 1
+            nums[i] = -1 * abs(nums[i])
+        
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                res.append(i + 1)
+        return res
+        
+        
