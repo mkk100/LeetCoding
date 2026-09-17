6f103220fa6cf934065a349b68cf4e4de714e4f5
mkk100 <tommychimes2003@gmail.com>
2026-09-17 09:06:08 -0700

    alt approach

--- 2026/LeetCodeExplore/Stacks&Queues/dailyTemperatures.py
+++ 2026/LeetCodeExplore/Stacks&Queues/dailyTemperatures.py
@@ -8,8 +8,24 @@ class Solution(object):
         res = [0] * len(temperatures)
         for i in range(len(temperatures)):
             while stack and temperatures[i] > stack[-1][1]:
-                index, value = stack.pop()
+                index = stack.pop()
                 res[index] = i - index
             stack.append([i,temperatures[i]])
+        return res
+
+# faster approach where you only use index instead of index value pair
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        stack = []
+        res = [0] * len(temperatures)
+        for i in range(len(temperatures)):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                index = stack.pop()
+                res[index] = i - index
+            stack.append(i)
         return res
         
