ed768eb699114bcce2f13c0ec32cc5e7c3ed6b68
mkk100 <tommychimes2003@gmail.com>
2026-09-24 10:34:46 -0700

    leet hard

--- 2026/LeetCodeExplore/BinaryTrees/serializeDeserializeBinTree.py
+++ 2026/LeetCodeExplore/BinaryTrees/serializeDeserializeBinTree.py
@@ -52,3 +52,66 @@ class Codec:
 # ser = Codec()
 # deser = Codec()
 # ans = deser.deserialize(ser.serialize(root))
+
+
+# bfs
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        res = []
+        q = deque([root])
+        while q:
+            node = q.popleft()
+            if node:
+                res.append(str(node.val))
+                q.append(node.left)
+                q.append(node.right)
+            else:
+                res.append("N")
+
+        return ",".join(res)
+            
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        data = data.split(",")
+        if data[0] == "N":
+            return None
+            
+        root = TreeNode(int(data[0]))
+        q, i = deque([root]), 1
+        
+        while q and i < len(data):
+            node = q.popleft()
+            
+            if data[i] != "N":
+                node.left = TreeNode(int(data[i]))
+                q.append(node.left)
+            i += 1
+            if data[i] != "N":
+                node.right = TreeNode(int(data[i]))
+                q.append(node.right)
+            i += 1 
+
+        return root
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
